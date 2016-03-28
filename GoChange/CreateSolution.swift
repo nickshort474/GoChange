b2f83237ef95dbe29a307e7832b5fc836eb9226c
Nick Short <nickshorty2010@yahoo.co.uk>
2016-03-28 17:27:05 +0100

    saved solutions to firebase, streamlined save to core data into one file, got basic tableview follwoing page setup

---
+++ GoChange/CreateSolution.swift
@@ -0,0 +1,9 @@
+//
+//  CreateSolution.swift
+//  GoChange
+//
+//  Created by Nick Short on 25/03/2016.
+//  Copyright © 2016 Nick Short. All rights reserved.
+//
+
+import Foundation
---
+++ GoChange/Tweak.swift
@@ -0,0 +1,9 @@
+//
+//  Tweak.swift
+//  GoChange
+//
+//  Created by Nick Short on 25/03/2016.
+//  Copyright © 2016 Nick Short. All rights reserved.
+//
+
+import Foundation
